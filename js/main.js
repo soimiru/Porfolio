8d4905b5e2cba446a23c5093d2dd87d7e19e83ec
Sonia <so.milagro.2018@alumnos.urjc.es>
2022-01-30 16:25:05 +0100

    Version 3.0

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+$(document).ready(function () {
+
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+
+  let barScroll = false;
+
+  function isScrolledIntoView(elem)
+  {
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+  
+      var elemTop = $(elem).offset().top;
+      var elemBottom = elemTop + $(elem).height();
+  
+      return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+  }
+  
+
+  
+  $('.barra').each(function () {
+      $(this).find('.barra-progress').animate({
+          width: $(this).attr('data-percent')
+      }, 5000);
   });
+  barScroll = true;
+      
+  
+ 
+});
+
+
